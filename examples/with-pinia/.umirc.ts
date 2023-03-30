8f37e5feb565964f4345d105cd8cabef9366d059
xiaohuoni <xiaohuoni@dingtalk.com>
2023-03-30 22:35:39 +0800

    feat: demo with pinia

---
+++ examples/with-pinia/.umirc.ts
@@ -0,0 +1,18 @@
+export default {
+  lowImport: {
+    libs: [
+      {
+        importFrom: 'vue',
+        members: ['computed', 'ref'],
+      },
+      {
+        importFrom: 'pinia',
+        members: ['defineStore'],
+      },
+      {
+        importFrom: '@/stores/count',
+        members: ['useCounterStore'],
+      },
+    ],
+  },
+};
