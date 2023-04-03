baf0f57f2200fbe8d0d08a7ebdd885d55edbb8d4
xiaohuoni <xiaohuoni@dingtalk.com>
2023-04-03 18:07:13 +0800

    feat: add Layout api

--- packages/layout/src/index.ts
+++ packages/layout/src/index.ts
@@ -7,4 +7,16 @@ export default (api: IApi) => {
   // 注意安装的包要装到这个包里，项目中没有直接依赖的，记得删除
   // 在  api.onGenerateFiles 的时候，把 layout 文件，写到临时文件夹里面。
   // 感觉像是将 examples/with-layout/layouts/index.vue 复制到 examples/with-layout/.umi/plugin-layout/index.vue
+
+  // 使用 api.addLayouts 用 examples/with-layout/.umi/plugin-layout/index.vue 包裹项目
+  
+  // api.addLayouts(() => {
+  //   return [
+  //     {
+  //       id: 'vue-layout',
+  //        这里要注意路径
+  //       file:index.vue,
+  //     },
+  //   ];
+  // });
 };
