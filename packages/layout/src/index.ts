10a0a37395f292ae66e9a78793d4ff846be01745
雷冬梅 <leidongmei@xmfunny.com>
2023-04-04 17:47:04 +0800

    feat(web): plugin-layout support

--- packages/layout/src/index.ts
+++ packages/layout/src/index.ts
@@ -9,7 +9,6 @@ const layoutMap = {
   mobile: 'mobileLayout.tpl',
 };
 export default (api: IApi) => {
-  // 把 examples/with-layout/plugin.ts 里面的内容移到这里
   api.addRuntimePluginKey(() => ['mobileLayout']);
   api.modifyViteConfig((config) => {
     config.plugins?.push(
@@ -27,11 +26,6 @@ export default (api: IApi) => {
     );
     return config;
   });
-  // 然后在 with-layout 里面安装 @alitajs/vue-layout 并在 配置文件中（examples/with-layout/.umirc.ts） 写明
-  // plugins:[' @alitajs/vue-layout'] //TODO:这个是用于做什么的？
-  // 注意安装的包要装到这个包里，项目中没有直接依赖的，记得删除 // TODO:要删除什么？
-  // 在  api.onGenerateFiles 的时候，把 layout 文件，写到临时文件夹里面。
-  // 感觉像是将 examples/with-layout/layouts/index.vue 复制到 examples/with-layout/.umi/plugin-layout/index.vue
   api.onGenerateFiles(() => {
     const layoutTpl = readFileSync(
       // 读取到layout文件内容
@@ -49,11 +43,7 @@ export default (api: IApi) => {
     return [
       {
         id: 'vue-layout',
-        file: join(
-          api.paths.absTmpPath,
-          api.plugin.key ? `plugin-${api.plugin.key}` : '',
-          join(DIR_NAME, 'index.vue'),
-        ),
+        file: join(api.paths.absTmpPath, join(DIR_NAME, 'index.vue')),
       },
     ];
   });
