1e7bc5ffb51cec1bf5d96bbd7323c0fbecbabdbc
xiaohuoni <xiaohuoni@dingtalk.com>
2023-04-10 16:41:13 +0800

    chore: remove unused version

--- packages/valita/src copy/constants.ts
+++
@@ -1,3 +0,0 @@
-export const MIN_NODE_VERSION = 14;
-export const DEV_COMMAND = 'dev';
-export const FRAMEWORK_NAME = 'valita';
--- packages/valita/src copy/plugins/vaAppData.ts
+++
@@ -1,9 +0,0 @@
-import { IApi } from 'umi';
-import { FRAMEWORK_NAME } from '../constants';
-
-export default (api: IApi) => {
-  api.modifyAppData((memo) => {
-    memo.umi.name = FRAMEWORK_NAME;
-    return memo;
-  });
-};
