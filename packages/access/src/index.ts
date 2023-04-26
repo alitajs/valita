dfcf7c20a3c43ff8fd19bcd8b840909aaf85df01
ChaconneLuo <asgardannila@gmail.com>
2023-04-26 18:48:36 +0800

    feat: init access

--- packages/access/src/index.ts
+++ packages/access/src/index.ts
@@ -1,6 +1,30 @@
+import { join } from 'path';
 import { IApi } from 'valita';
 const DIR_NAME = 'plugin-access';
 
 export default (api: IApi) => {
-  
+  api.describe({
+    key: 'access',
+    config: {
+      schema({ zod }) {
+        return zod.map(zod.string(), zod.array(zod.string()));
+      }
+    },
+    enableBy: api.EnableBy.config,
+  });
+  api.writeTmpFile({
+    path: join(DIR_NAME, 'index.ts'),
+    noPluginDir: true,
+    content: ``
+  })
+  api.writeTmpFile({
+    path: join(DIR_NAME, 'runtime.ts'),
+    noPluginDir: true,
+    content: ``
+  })
+  api.writeTmpFile({
+    path: join(DIR_NAME, 'types.d.ts'),
+    noPluginDir: true,
+    content: ``
+  })
 };
